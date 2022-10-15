71f1a744797e3aa6bfc9fbf8ca98211174bdb040
FolderMaster <andrey.pchelintsev.02@mail.ru>
2022-10-15 16:37:05 +0700

    Around of end of lab work №2.

---
+++ src/ObjectOrientedPractics/Model/Store.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Магазин с товарами и покупателями.
+    /// </summary>
+    public class Store
+    {
+        /// <summary>
+        /// Товары.
+        /// </summary>
+        private List<Item> _items = new List<Item>();
+
+        /// <summary>
+        /// Покупатели.
+        /// </summary>
+        private List<Customer> _customers = new List<Customer>();
+
+        /// <summary>
+        /// Возращает и задаёт товары.
+        /// </summary>
+        public List<Item> Items
+        {
+            get => _items;
+            set => _items = value;
+        }
+
+        /// <summary>
+        /// Возращает и задаёт товары.
+        /// </summary>
+        public List<Customer> Customers
+        {
+            get => _customers;
+            set => _customers = value;
+        }
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Adress"/> по-умолчанию.
+        /// </summary>
+        public Store()
+        {
+        }
+    }
+}
