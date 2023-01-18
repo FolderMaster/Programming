4a685c1fca9dfd11e9b1ee741d34d70745aa0c1f
FolderMaster <andrey.pchelintsev.02@mail.ru>
2023-01-18 16:17:03 +0700

    Individual tasks.

---
+++ IndividualTask1/Program.cs
@@ -0,0 +1,12 @@
+﻿using IndividualTask1;
+
+List<Armour> equipment = new List<Armour>()
+{
+    new Armour("Шлем", ArmourType.Helmet, 10),
+    new Armour("Поножи", ArmourType.LegArmor, 30),
+    new Armour("Перчатки", ArmourType.Gloves, 5),
+    new Armour("Кольчуга", ArmourType.LegArmor, 15)
+};
+
+Warior warior = new Warior("Олег", 20, 5, equipment);
+warior.ShowInfo();
